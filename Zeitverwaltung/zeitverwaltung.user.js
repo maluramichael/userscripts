aa85092e6a6440f1daa80be6d0cc821a6703839e
Michael Malura <michael.malura@socialbit.de>
2016-03-31 16:14:05 +0200

    Add customizable variables

--- Zeitverwaltung/zeitverwaltung.user.js
+++ Zeitverwaltung/zeitverwaltung.user.js
@@ -9,11 +9,14 @@
 // ==/UserScript==
 /* jshint -W097 */
 
+var nettoFactor = 0.6;
+var euroPerHour = 15;
+
 var timeCells = $('tr>td:last-child.color');
 
 // filter week and free days
 timeCells = timeCells.filter(function(){
-    return ($(this).hasClass('green') || $(this).hasClass('red'));
+    return ($(this).hasClass('green')) || ($(this).hasClass('red'));
 });
 
 // extract the times
@@ -28,7 +31,8 @@ var times = timeString.split(' ').map(function(time){
 
     var result = {
         hours: parsedTime.hours,
-        minutes: parsedTime.minutes
+        minutes: parsedTime.minutes,
+        brutto: parsedTime.hours * euroPerHour + (euroPerHour / 60 * parsedTime.minutes)
     };
 
     if (parsedTime.hours >= 8 &&
@@ -49,6 +53,8 @@ var times = timeString.split(' ').map(function(time){
             hours: (8 - parsedTime.hours) - 1,
             minutes: 60 - parsedTime.minutes,
         }
+
+        console.log(parsedTime, result.lessWork);
     }
 
     return result;
@@ -82,13 +88,14 @@ var noWork = {
 
 var overallWork = {
     hours: 0,
-    minutes: 0
+    minutes: 0,
+    brutto: 0
 };
 
 function addTime(timeObject, add){
     return {
         hours: timeObject.hours += add.lessWork.hours,
-        minutes: timeObject.minutes += add.lessWork.minutes
+        minutes: timeObject.minutes += add.lessWork.minutes,
     }
 }
 
@@ -97,6 +104,7 @@ times.forEach(function(time){
     if (time.overWork){
         overWork.hours += time.overWork.hours;
         overWork.minutes += time.overWork.minutes;
+        overWork.brutto += time.overWork.brutto;
     } else if (time.noWork){
         noWork.hours += time.noWork.hours;
         noWork.minutes += time.noWork.minutes;
@@ -106,12 +114,15 @@ times.forEach(function(time){
     }
     overallWork.hours += time.hours;
     overallWork.minutes += time.minutes;
+    overallWork.brutto += time.brutto;
 });
 
 function getHoursAndMinutesFromMinutes(timeObject){
     return {
         hours: timeObject.hours + ((timeObject.minutes - (timeObject.minutes%60))/60),
-        minutes: timeObject.minutes%60
+        minutes: timeObject.minutes%60,
+        brutto: timeObject.brutto,
+        netto: timeObject.brutto * nettoFactor
     }
 }
 
@@ -124,7 +135,7 @@ overallWork = getHoursAndMinutesFromMinutes(overallWork);
 console.log('over work', 'hours', overWork.hours, 'minutes', overWork.minutes);
 console.log('under work', 'hours', lessWork.hours, 'minutes', lessWork.minutes);
 console.log('no work', 'hours', noWork.hours, 'minutes', noWork.minutes);
-console.log('overall work', 'hours', overallWork.hours, 'minutes', overallWork.minutes);
+console.log('overall work', 'hours', overallWork.hours, 'minutes', overallWork.minutes, 'brutto', overallWork.brutto, 'netto', overallWork.netto);
 
 document.title = todayWork.hours + ':' + todayWork.minutes + ' (' + overWork.hours + ':' + overWork.minutes + ')';
 setTimeout(function(){location.reload()}, 1000 * 60);
